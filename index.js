df5ef330678e824281dac50cb62f26ec997227bc
joao marcelo <joao.ferreira@nodis.com.br>
2022-03-11 18:17:36 -0300

    Desenvolvimento BackEnd

---
+++ index.js
@@ -0,0 +1,19 @@
+let data = new Date();
+
+let hora = data.getHours();
+
+let min = data.getMinutes();
+
+let saudacao;
+
+if (hora <= 11){
+    saudacao = "BOM DIA."; 
+} else if (hora <= 17){
+    saudacao = "BOA TARDE."
+} else {
+    saudacao = "BOA NOITE."
+}
+
+console.log("Olá! " + saudacao)
+
+console.log("Agora São " + hora + "Horas e" + min + "minutos" )
