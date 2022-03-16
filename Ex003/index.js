21ed32d3c0b07ee93285395a0b31a50af875f97d
joao marcelo <joao.ferreira@nodis.com.br>
2022-03-16 11:47:06 -0300

    Mudança de Api para ApiRest

--- Ex003/index.js
+++ Ex003/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+let data = new Date();
+
 app.get('/',(req, res) => {
     let mensagem = {mensagem: 'ola, mundo!'};
     res.json(mensagem);
 });
 
 app.listen(8080, () => {
-    let data = new Date();
+    
     console.log('Servidor Node iniciado em. ' + data);
 });
