c6ee91de8ddb1e8b0361de849038e00b16abdd0b
joao marcelo <joao.ferreira@nodis.com.br>
2022-03-16 11:26:36 -0300

    Atualizaçao da API para API REST

--- Ex003/index.js
+++ Ex003/index.js
@@ -1,13 +1,13 @@
-const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get('/',(req, res) =>{
-    response.send('Hello,Word!');
+app.get('/',(req, res) => {
+    let mensagem = {mensagem: 'ola, mundo!'};
+    res.json(mensagem);
 });
 
-app.listen(8080, () =>{
+app.listen(8080, () => {
     let data = new Date();
-    console.log('Servidor Node iniciado em. ' + data)
+    console.log('Servidor Node iniciado em. ' + data);
 });
