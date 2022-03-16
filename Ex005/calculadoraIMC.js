4723d351334c21a9e82091afd2fb91bfcfd994cf
joao marcelo <joao.ferreira@nodis.com.br>
2022-03-16 15:33:38 -0300

    Calculadora IMC

--- Ex005/calculadoraIMC.js
+++ Ex005/calculadoraIMC.js
@@ -3,4 +3,28 @@ function efetuarCalculoIMC(peso, altura){
     return imc;
 }
 
+function retornaStatusIMC(imc)
+{
+    let status;
+
+    if (imc < 18.5)
+{
+    status = 'Abaixo do peso';
+}
+else if (imc >= 18.5 && imc < 24.9)
+{
+    status = 'Peso normal';
+}
+else if(imc >= 24.9 && imc < 30)
+{
+    status = 'Acima do peso';
+}
+else
+{
+    status = 'Obeso';
+}
+return status;
+}
+
 exports.efetuarCalculoIMC = efetuarCalculoIMC;
+exports.retornaStatusIMC = retornaStatusIMC;
--- Ex005/index.js
+++ Ex005/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 
-const efetuarCalculoIMC = require('./calculadoraIMC'); 
+const calculadoraIMC = require('./calculadoraIMC'); 
 
 app.get('/', (req,res) => {
 
     let peso = req.query.peso;
     let altura = req.query.altura;
 
-    let imc = efetuarCalculoIMC.efetuarCalculoIMC(peso,altura);
+    let imc = calculadoraIMC.efetuarCalculoIMC(peso,altura);
+    let status = calculadoraIMC.retornaStatusIMC(imc);
 
-    res.json({imc : imc});
+    let json = {imc: imc, status: status};
+
+    res.json(json);
 });
 app.listen(8080 ,() => {
     let data = new Date();
-    console.log('Servidor node iniciado em' +data);
+    console.log('Servidor node iniciado em' + data);
 });
+
