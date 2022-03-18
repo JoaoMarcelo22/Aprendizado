d6a3072f0c73144a9e0d5b56409f3eef620a2f73
joao marcelo <joao.ferreira@nodis.com.br>
2022-03-18 18:50:55 -0300

    Exercicios em C# e .NET

---
+++ Ex008/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex008
+{
+    class program 
+    {
+        static void main(string[] args)
+        {
+            int? idade = 0;
+            Console.WriteLine(idade);
+            idade = null;
+            Console.WriteLine(idade);
+            idade = 25;
+            Console.WriteLine(idade);
+        }
+    }
+}
