648b5398cbf33d80ebc740b29682853d4c3d0708
joao marcelo <joao.ferreira@nodis.com.br>
2022-03-21 15:52:48 -0300

    Todos os Exercicios basicos de C#

--- Ex009/conversão.cs
+++ Ex009/conversão.cs
@@ -6,6 +6,8 @@ namespace Ex009
     {
         static void main(string[] args)
         {
+            //METODOS DE CONVERSAÇÃO----------------------------------
+
             int inteiro = 100;
             float real = 25.5f;
 
@@ -17,9 +19,39 @@ namespace Ex009
             inteiro = int.Parse(valorReal); //conversão Parse precisa receber uma string
 
             inteiro = Convert.ToInt32(real); //conversão Convert converte todos os valores primitivos
-            
-               
 
+
+
+
+            //OPERADORES----------------------------------------------
+
+            double x = 2;
+            x = 75-23.8;                    //subtração
+            Console.WriteLine(x);
+            x = 75 + 23;                    //soma
+            x = 75 * 23;                    //multiplicação
+            x = 75 / 23;                    //divisão
+            x++;                            //acrecenta um
+            x--;                            //subtrai um
+
+            //ESTRUTURA CONDICIONAL--------------IF/ELSE-------------
+            int idade = 72;
+
+            if(idade <18){
+                Console.WriteLine("ADOLECENTE");   //caso seja verdadeiro
+            }
+            else if((idade<=29)&&(idade>=18)){     //senão,testa outra condição
+                Console.WriteLine("JOVEM ADULTO");
+            }
+            else if((idade<40)&&(idade>=30)){      //senão,testa outra condição
+                Console.WriteLine("ADULTO");
+            }
+            else if((idade<=65)&&(idade>40)){      //senão,testa outra condição
+                Console.WriteLine("SENHOR");
+            }
+            else{
+                Console.WriteLine("IDOSO");        //senão.
+            }
         }
     }
 }
