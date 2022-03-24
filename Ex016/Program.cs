060d1473fd76646e01e62c9b67c181145843bb16
joao marcelo <joao.ferreira@nodis.com.br>
2022-03-24 17:57:10 -0300

    Formataçao de moedas em C#

---
+++ Ex016/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10536.25m;
+            
+            Console.WriteLine(
+                valor.ToString(
+                    // "F" tem uma precisão maior
+                    // "N"  formato de numero apenas
+                    // "P" formata para porcentagem
+                    "C",  //converte para a moeda 
+                    CultureInfo.CreateSpecificCulture("es-ES") // de onde vem a moeda
+                )
+            );
+        Math.Round(valor); // faz o arrendondamento
+        Math.Ceiling(valor); // arrendonda sempre para cima
+        Math.Floor(valor); // sempre arredonda para baixo
+        }
+    }
+}
