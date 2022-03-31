4b12609ee08927e95563f6f1ae12a48729ae2c40
joao marcelo <joao.ferreira@nodis.com.br>
2022-03-31 12:17:42 -0300

    Exercicios em JS Orientados a Objetos

---
+++ Ex020/index.js
@@ -0,0 +1,22 @@
+// Boas praticas as classes São no maximo 1 por arquivo
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
