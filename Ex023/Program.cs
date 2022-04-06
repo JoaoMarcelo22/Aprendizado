37d35c6c96614a6d788e6688e2a7745304ebe462
joao marcelo <joao.ferreira@nodis.com.br>
2022-04-06 12:23:19 -0300

    Exercicio C# Ex23

---
+++ Ex023/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex023{
+    class Program{
+        static void Main(string[] Args){
+            Menu();
+        }
+        static void Menu(){
+            int idade = 0;
+            int Qtd = 0;
+            int i = 0;
+            Console.WriteLine("Verificador de idade");
+            Console.WriteLine("--------------------");
+            while (i<10)
+            {
+                i++;
+                Console.WriteLine("Digite a idade : ");
+                idade = Convert.ToInt32(Console.ReadLine());
+                if(idade>=18){
+                    Qtd++;
+                }
+            }
+            Console.WriteLine($"Existem {Qtd} pessoas com 18 anos ou mais.");
+            Console.ReadKey();
+            Console.Clear();
+            Menu();
+        }
+    }
+}
